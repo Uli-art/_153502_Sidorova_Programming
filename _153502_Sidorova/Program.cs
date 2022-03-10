717d144a0e8904d62916561ec803da52e39df6c1
Uli-art <78424200+Uli-art@users.noreply.github.com>
2022-03-10 10:35:54 +0300

    Add project files.

---
+++ _153502_Sidorova/Program.cs
@@ -0,0 +1,11 @@
+// See https://aka.ms/new-console-template for more information
+Console.Write("Enter first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber / secondNumber;
+Console.WriteLine("Result: " + result);
+
+
